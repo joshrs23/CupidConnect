c3c09bf946a7e78107460a68b0b1536de63bb838
EmilianoOnTheRunn <emilianoruizgomez@outlook.com>
2023-09-22 15:23:32 -0400

    first commit login

--- pages/index.vue
+++ pages/index.vue
@@ -6,20 +6,34 @@
           <h1 class="text-9xl font-bold gradient-title">&nbsp;&nbsp;CupidConnect Index</h1>
           <p class="text-4xl mb-8 font-bold "> hola prueba cupid 1.</p>
         </div>
+        <div class="column-2 sm: column-3 gap-6">
+            <input type="text" class="username" name="username" value="Username">
+            <input type="text" class="password" name="username" value="Password">
+          </div>
       </div>
       <div class="h-96 min-w-screen shadow-inner shadow-lg shadow-black/50 bg-black/75 flex">
         <div class="w-1/2 h-full flex items-center justify-center">
-          <div class="text-5xl text-left w-3/4">
-            consigue parejas con nostros
-          </div>
+          
+          
         </div>
       </div>
     </div>
   </div>
+
 </template>
+
 
 <style scoped>
 
+  .mb-6{
+      display: grid;
+    }
+    .username{
+      background-color: black;
+    }
+    .password{
+      background-color: black;
+    }
 </style>
 
   <script>

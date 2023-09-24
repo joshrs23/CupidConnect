6142db91bc708936b8c961788301c397fcc46fcb
EmilianoOnTheRunn <emilianoruizgomez@outlook.com>
2023-09-23 22:17:48 -0400

    changes to the login view

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import modules from './modules'
 
+
 export default ({
   ...modules,
   serverMiddleware: [
@@ -14,6 +15,26 @@ export default ({
     // modules: ['navigation', 'pagination'], // all modules are imported by default
   },
   css: [
-    '@fortawesome/fontawesome-free/css/all.css'
+    '@fortawesome/fontawesome-free/css/all.css',
   ],
+  buildModules: [
+    // Add the @nuxtjs/google-fonts module
+    '@nuxtjs/google-fonts',
+  ],
+  googleFonts: {
+    families: {
+      'Lovers Quarrel': true,
+    },
+    display: 'swap', // Specify your desired display option
+  },
+  head: {
+    // Add the Google Fonts <link> tag here
+    link: [
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Lovers+Quarrel&display=swap',
+      },
+    ],
+  },
 })
+
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,19 +1,18 @@
-<link href="https://fonts.googleapis.com/css2?family=Lovers+Quarrel&display=swap" rel="styleLang">
-
+@import url('https://fonts.googleapis.com/css2?family=Lovers+Quarrel&display=swap');
+/images/arrow-right.png
+/assets/styles.css;
+ 
 <template>
     <div class="container mx-auto mt-4">
-  <div class="grid grid-cols-12 gap-4">
-    <!-- First Column (7 columns wide) -->
-    <div id="logo" class="col-span-7 bg-black-200 p-4 flex justify-center">
+  <div class="grid grid-cols-12 gap-5">
+    <div id="logo" class="col-span-7 bg-black-200 p-4 flex justify-center ">
       <h2 class="text-xl font-semibold">LOGO</h2>
-     
     </div>
 
-    <!-- Second Column (5 columns wide)
-    <div id="content" class="col-span-5 bg-black p-4">
+
+    <div id="content" class="col-span-5 bg-black p-4 border-l border-white h-full">
       <h2 id="Login" class="text-xl font-semibold text-white flex flex-col justify-center items-center ">Log in</h2>
 
-      <!-- Form for Sign Up -->
       <form>
         <div class="mb-4">
           <input type="text" id="username" name="username" class="form-input mt-1 block w-full rounded-md border-black-300" placeholder="Username">
@@ -22,10 +21,19 @@
         <div class="mb-4">
           <input type="password" id="password" name="password" class="form-input mt-1 block w-full rounded-md border-gray-300" placeholder="Password">
         </div>
+         <button id="arrow-right" type="submit">
+                <img src="@/images/arrow-right.png" alt="arrowIcon" />
+          </button>
 
-        <div id="btnRegister" class="flex justify-center"> <!-- Added this flex container for centering -->
-            <button  type="submit" class="bg-black-200 text-white">Register -></button>
+          <div id="btnRegister" class="flex justify-center items-center mb-4">
+            <label for="registerButton" class="cursor-pointer">
+              <button id="registerButton" type="submit" class="bg-black-200 text-white flex items-center">
+                Register
+                <img src="@/images/arrow-right.png" alt="arrowIcon" class="mx-2" />
+              </button>
+            </label>
           </div>
+   
       </form>
     </div>
   </div>
@@ -34,21 +42,35 @@
 
 
 <style>
+    #content{
+      margin-left: 10em;
+    }
+    #arrow-right{
+      margin-left: 20em;
+    }
     div{
-      font-family: serif;
+      font-family: 'Lovers Quarrel', serif;
+      background-color: black;
     }
     #logo{
-        margin-top: 15em;
+        padding-top: 15em;
+        background-color: black;
+        color: white;
+    }
+    icon {
+    font-size: 24px;
+    color: white;
     }
     
-#Login{
-       margin-top: 3em;
+    #Login{
+       padding-top: 2em;
         font-size: 2.5em;
         font-family: 'Lovers Quarrel', serif;
     }
     #btnRegister{
       font-size: 1.5em; 
-      margin-top: 14em;
+      margin-top: 10em;
+      margin-left: 2.5em;
     }
 
     form{

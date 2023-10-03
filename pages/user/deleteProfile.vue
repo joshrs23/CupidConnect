31c0e1ec120f01acf673ca2f222d3a4227f34e35
EmilianoOnTheRunn <emilianoruizgomez@outlook.com>
2023-10-02 21:57:48 -0400

    deleteProfile view

--- pages/user/deleteProfile.vue
+++ pages/user/deleteProfile.vue
@@ -1,21 +1,50 @@
+<link href="https://fonts.googleapis.com/css2?family=Lovers+Quarrel&display=swap" rel="stylesheet">
+
 <template>
-  <div>
-    <h1>Hello, {{ message }}</h1>
+  <div class="grid col-4 flex items-center justify-center h-screen text-white">
+    
+    <div class="white font-48 text-9xl">
+      <h1>Delete Profile</h1>
+    </div>
+
+    <div class="mx-40 text-lg">
+        <label>
+          <span class="block  font-medium"> Please enter username: </span>
+          <input class="form-input border-black-300  mt-3 bg-black border border-gray"
+          type="text" id="username" name="username" placeholder="Username" v-model="username" required>
+
+        </label>
+    </div>
+
+    <div class="w-32 font-medium hover:bg-orange-700 p-5 text-lg flex justify-center border border-red-500 p-5">
+      <button @click="deleteMessage"> Delete </button>
+    </div>
+      <p class="text-red-500">{{message}}</p>
+    
+
   </div>
+
+
 </template>
 
 <script>
 export default {
   data() {
     return {
-      message: 'you have deleted your profile '
+      message: '',
     };
-  }
-}
+  },
+  methods: {
+
+    deleteMessage(){
+      this.message = 'You have deleted your profile. Bye bye!';
+    }
+  },
+};
 </script>
 
 <style scoped>
-h1 {
-  color: blue;
-}
+  h1 {
+      font-family: 'Lovers Quarrel', serif;
+  }
 </style>

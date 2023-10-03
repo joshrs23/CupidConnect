1a40043d3768e60aa807f54d76f94c820848d055
jose salazar <joshrs23@gmail.com>
2023-10-03 01:22:17 -0400

    connection front backend for delete user

--- pages/user/deleteProfile.vue
+++ pages/user/deleteProfile.vue
@@ -3,57 +3,157 @@
 <template>
   <div class=" grid col-4 flex items-center justify-center h-screen  text-white">
     
-    <div class="white mx-40 text-5xl lg:text-9xl">
+        <div class="white mx-40 text-5xl lg:text-9xl">
 
-      <h1>Delete Profile</h1>
+            <h1>Delete Profile</h1>
 
-    </div>
+        </div>
 
-    <div class="mx-40 lg:mx-72  md:text-md lg:text-lg">
+        <div class="mx-40 lg:mx-72  md:text-md lg:text-lg">
 
-        <label>
+            <label>
 
-          <span class="block font-medium"> Please to delete enter username: </span>
+                <span class="block font-medium"> Please to delete enter username: </span>
 
-          <input class="form-input border-black-300  mt-3 bg-black border border-gray"
-          type="text" id="username" name="username" placeholder="Username" v-model="username" required>
+                <input class="form-input border-black-300  mt-3 bg-black border border-gray"
+                type="text" id="username" name="username" placeholder="Username" v-model="username" required>
 
-        </label>
+            </label>
 
-    </div>
+        </div>
 
         <div class="w-32 mx-40 lg:mx-80  font-medium hover:bg-gray-700 p-5 text-lg flex justify-center border border-white p-5">
 
-        <button @click="deleteMessage"> Delete </button>
+            <button @click="deleteMessage"> Delete </button>
 
-    </div>
+        </div>
 
-    <p class="text-white mx-40 lg:mx-64">{{message}}</p>
+        <!-- error -->
+        <div v-if="error" class="text-red-500 text-center my-4">
+          {{ error }}
+        </div>
     
-  </div>
+    </div>
 
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      message: '',
-    };
-  },
-  methods: {
+import { useUserStore } from '../../stores/users'
+import axios from 'axios';
 
-    deleteMessage(){
-      this.message = 'You have deleted your profile. Bye bye!';
-    }
-  },
-};
+    export default {
+
+        data() {
+
+            return {
+
+              username : '',
+              error : '',
+              userData : useUserStore(),
+
+            };
+
+        },
+        methods: {
+
+            async deleteMessage(){
+
+                if(this.verify()){
+
+                    const _userId = localStorage.getItem('CupidConnectId'); 
+                    const token = localStorage.getItem('CupidConnectToken'); 
+                    console.log(_userId);
+                    const dataf = {
+
+                        userId: _userId,
+
+                    };
+
+                    const response = await axios.post('https://espacionebula.com:8000/delete-user', dataf, {
+
+                        headers: {
+
+                            'Access-Control-Allow-Origin': '*',
+                            'Authorization': `Bearer ${token}`,
+
+                        },
+                        mode: 'cors',
+
+                    });
+                    debugger;
+                    if(response.data.success){
+
+                        this.userData.logOutUser();
+                        this.$router.push('/delete');
+
+                    }else{
+
+                        this.error = response.data.error;
+                        console.log(response.data.error);
+                        this.clearErrorMessageAfterDelay();
+
+                    }
+
+                }else{
+
+                    this.clearErrorMessageAfterDelay();
+
+                }
+                
+
+            },
+            verify(){
+
+                if(!this.username){
+
+                    this.error = "Please write your username";
+                    console.log("Please write your username");
+                    return false;
+
+                }
+
+                if(this.username.length < 10){
+
+                    this.error = "Your username must have at least 10 characters";
+                    console.log("Your username must have at least 10 characters");
+                    return false;
+
+                }
+
+                if(this.username != localStorage.getItem('CupidConnectuser')){
+
+                    this.error = "Error "+this.username+" is not your user name";
+                    console.log("Error "+this.username+" is not your user name");
+                    return false;
+
+                }
+
+                return true;
+
+            },
+            clearErrorMessageAfterDelay() {
+          
+                setTimeout(() => {
+
+                    this.error = "";
+
+                }, 5000);
+
+            },
+
+
+        },
+
+    };
 </script>
 
 <style scoped>
+
   h1 {
-      font-family: 'Lovers Quarrel', serif;
-  }
+
+        font-family: 'Lovers Quarrel', serif;
+
+    }
 
 
 </style>
